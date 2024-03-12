39783e6cc4ea78a85f74d242b1ca1e7380befb90
INFOPULSE\Roman.Nepomniashchyi <roman.nepomniashchyi@infopulse.com>
2024-03-12 21:57:44 +0200

    repeat and understanding

--- LeetCode/LeetCode/LongestCommonPrefix.cs
+++ LeetCode/LeetCode/LongestCommonPrefix.cs
@@ -1,13 +1,14 @@
 ﻿namespace LeetCode;
 public class LongestCommonPrefix
 {
-    public string LongestCommonPrefixMethod1(string[] strs)
+    public string LongestCommonPrefixMethod(string[] strs)
     {
         if (strs == null || strs.Length == 0)
+        {
             return "";
+        }
 
-        string prefix = strs[0];
-
+        var prefix = strs[0];
         for (int i = 1; i < strs.Length; i++)
         {
             while (strs[i].IndexOf(prefix) != 0)
@@ -19,38 +20,6 @@ public class LongestCommonPrefix
         }
 
         return prefix;
-    }
-
-    public string LongestCommonPrefixMethod(string[] strs)
-    {
-        if (strs == null || strs.Length == 0)
-        {
-            return "";
-        }
-
-        var result = "";
-        var first =strs.First();
-        var source = strs.ToList();
-        foreach (var s in source)
-        {
-            if (s != first)
-            {
-                for (int i = 0; i < s.Length; i++)
-                {
-                    if (i <= s.Length)
-                    {
-                        if (s[i] == first[i])
-                        {
-                            result += s[i];
-
-                        }
-                    }
-                    
-                }
-            }
-        }
-
-        return result;
     }
 
 
